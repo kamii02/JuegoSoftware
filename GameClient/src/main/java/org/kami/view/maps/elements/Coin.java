5b9543d39099c91973f49fac7abfb721dfc7ae8a
kamii02 <camilaauto10@gmail.com>
2026-04-08 17:50:37 -0500

    The currency is added to the maps.

--- GameClient/src/main/java/org/kami/view/maps/elements/Coin.java
+++ GameClient/src/main/java/org/kami/view/maps/elements/Coin.java
@@ -10,7 +10,7 @@ import org.kami.view.maps.mapelementsfactory.IMapElement;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Coin implements IMapElement {
+public class Coin implements IMapElement, ITexturedElement {
     private int x;
     private int y;
     private int height;
@@ -18,10 +18,20 @@ public class Coin implements IMapElement {
     private int points = 20;
     private long spawnTime;
     private long duration = 7000;
+    private String texturePath;
 
     public boolean isExpired() {
         return System.currentTimeMillis() - spawnTime > duration;
     }
 
 
+    @Override
+    public void setTexturePath(String path) {
+        this.texturePath = path;
+    }
+
+    @Override
+    public String getTexturePath() {
+        return texturePath;
+    }
 }
